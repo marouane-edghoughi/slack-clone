718d616b5ba0750af93ec61678542941b1501c35
Marouane Edghoughi <marouane@edghoughi.com>
2022-03-13 23:07:45 +0100

    Add description and logo to Welcome message

--- src/components/Welcome/WelcomeToSlack.js
+++ src/components/Welcome/WelcomeToSlack.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 import {
-    WelcomeContainer
+    Wrapper,
+    WelcomeContainer,
+    SlackLogo,
+    SlackDescription
 } from './WelcomeToSlack.styled';
+import { Copyrights } from '../Login/Login.styled';
 
 function WelcomeToSlack() {
     return (
-        <WelcomeContainer>
-            <h1>Welcome To Slack!!</h1>
-        </WelcomeContainer>
+        <Wrapper>
+            <WelcomeContainer>
+                <h1>Welcome to</h1>
+                <SlackLogo
+                    src='https://firebasestorage.googleapis.com/v0/b/slack-clone-2022.appspot.com/o/images%2Fslack-logo.svg?alt=media&token=b94f96aa-776f-4363-9c36-8048636eb29b'
+                />
+                <SlackDescription>Slack brings all your team communication into one place, making it all instantly searchable and available wherever you go.</SlackDescription>
+                <Copyrights>
+                    &copy; powered by Marouane Edghoughi.
+                </Copyrights>
+            </WelcomeContainer>
+        </Wrapper>
     );
 }
 
--- src/components/Welcome/WelcomeToSlack.styled.js
+++ src/components/Welcome/WelcomeToSlack.styled.js
@@ -1,7 +1,37 @@
 import styled from 'styled-components';
 
-export const WelcomeContainer = styled.div`
+export const Wrapper = styled.div`
     display: flex;
+    width: 100%;
+    height: 100%;
     justify-content: center;
     align-items: center;
+`;
+
+export const WelcomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 50%;
+`;
+
+export const SlackLogo = styled.img`
+    width: 300px;
+    height: auto;
+    margin-top: 40px;
+    margin-bottom: 40px;
+    pointer-events: none;
+    -webkit-user-drag: none;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently supported by Chrome, Opera and Firefox */
+`;
+
+export const SlackDescription = styled.p`
+    margin-bottom: 10px;
 `;
